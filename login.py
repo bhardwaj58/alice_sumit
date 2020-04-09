32a0e598b18ed29ff73f299286ce9a3ee403fefc
bhardwaj58 <42745111+bhardwaj58@users.noreply.github.com>
2020-04-09 23:00:53 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,9 @@
+username = 'HO786'
+password = 'Bhardwaj@16'
+api_secret = 'Avrt7mMhEjs8hZFb3IKZybuDhsxwnNcmjSzRkrWGNogAtVv4hg4BRjsk3lXJdy5K'
+twoFA = 'sumit'
+
+
+
+https://ant.aliceblueonline.com/oauth2/auth?response_type=code&client_id=HO786&redirect_uri=http%3A%2F%2F127.0.0.1%2F&scope=orders&state=c30T2qLL92PggTBIzgjpKIi8JMWpt3&access_type=authorization_code
+
